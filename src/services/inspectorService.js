5094cb05e00c9f601d4e94dccbe616c361d8e516
Malcolm_Tyler <malcolm@malcsdomain.co.uk>
2025-04-29 13:17:33 +0100

    fix: Add authorization headers to inspectorService API calls for secure access

--- src/services/inspectorService.js
+++ src/services/inspectorService.js
@@ -1,36 +1,71 @@
 import axios from 'axios';
 import { baseUrl } from '../config';
 
+const getToken = () => {
+  const user = localStorage.getItem('user');
+  if (!user) return null;
+  try {
+    return JSON.parse(user).token;
+  } catch {
+    return null;
+  }
+};
+
 const getAll = async () => {
-    const response = await axios.get(`${baseUrl}/inspectors`);
-    return response.data;
+  const token = getToken();
+  const response = await axios.get(`${baseUrl}/inspectors`, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  return response.data;
 };
 
 const getById = async (id) => {
-    const response = await axios.get(`${baseUrl}/inspectors/${id}`);
-    return response.data;
+  const token = getToken();
+  const response = await axios.get(`${baseUrl}/inspectors/${id}`, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  return response.data;
 };
 
 const create = async (inspector) => {
-    const response = await axios.post(`${baseUrl}/inspectors`, inspector);
-    return response.data;
+  const token = getToken();
+  const response = await axios.post(`${baseUrl}/inspectors`, inspector, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  return response.data;
 };
 
 const update = async (id, inspector) => {
-    const response = await axios.put(`${baseUrl}/inspectors/${id}`, inspector);
-    return response.data;
+  const token = getToken();
+  const response = await axios.put(`${baseUrl}/inspectors/${id}`, inspector, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  return response.data;
 };
 
 const remove = async (id) => {
-    await axios.delete(`${baseUrl}/inspectors/${id}`);
+  const token = getToken();
+  await axios.delete(`${baseUrl}/inspectors/${id}`, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
 };
 
 const inspectorService = {
-    getAll,
-    getById,
-    create,
-    update,
-    remove
+  getAll,
+  getById,
+  create,
+  update,
+  remove
 };
 
 export default inspectorService;
