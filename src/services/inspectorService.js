836e299f8a4534354b08122a5f3d829bb19c8581
Malcolm_Tyler <malcolm@malcsdomain.co.uk>
2025-04-29 13:04:59 +0100

    fix: Correct endpoint paths in inspectorService for consistency

--- src/services/inspectorService.js
+++ src/services/inspectorService.js
@@ -2,27 +2,27 @@ import axios from 'axios';
 import { baseUrl } from '../config';
 
 const getAll = async () => {
-    const response = await axios.get(`${baseUrl}/inspector`);
+    const response = await axios.get(`${baseUrl}/inspectors`);
     return response.data;
 };
 
 const getById = async (id) => {
-    const response = await axios.get(`${baseUrl}/inspector/${id}`);
+    const response = await axios.get(`${baseUrl}/inspectors/${id}`);
     return response.data;
 };
 
 const create = async (inspector) => {
-    const response = await axios.post(`${baseUrl}/inspector`, inspector);
+    const response = await axios.post(`${baseUrl}/inspectors`, inspector);
     return response.data;
 };
 
 const update = async (id, inspector) => {
-    const response = await axios.put(`${baseUrl}/inspector/${id}`, inspector);
+    const response = await axios.put(`${baseUrl}/inspectors/${id}`, inspector);
     return response.data;
 };
 
 const remove = async (id) => {
-    await axios.delete(`${baseUrl}/inspector/${id}`);
+    await axios.delete(`${baseUrl}/inspectors/${id}`);
 };
 
 const inspectorService = {
