2bc9c54295b71f14598d32f6ebf51bb62576a6f1
Malcolm_Tyler <malcolm@malcsdomain.co.uk>
2025-05-02 13:32:59 +0100

    feat: Refactor createUser to remove IsCustomer claim addition and retain emailConfirmed in the request

--- src/services/userService.js
+++ src/services/userService.js
@@ -21,19 +21,10 @@ const createUser = async (userData) => {
     lastName: userData.lastName,
     isCustomer: userData.isCustomer,
     customerId: customerId,
-    emailConfirmed: userData.emailConfirmed // Include emailConfirmed in the creation request
+    emailConfirmed: userData.emailConfirmed
   };
   
   const res = await apiClient.post('/Users', registerDto);
-  
-  // After creating the user, add the IsCustomer claim
-  if (res.data && res.data.id) {
-    await apiClient.post(`/Claims/${res.data.id}/claims`, {
-      type: "IsCustomer",
-      value: userData.isCustomer.toString()
-    });
-  }
-  
   return res.data;
 };
 
