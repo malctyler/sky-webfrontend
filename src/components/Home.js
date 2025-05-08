28eae1abbd5501b41f55a85de662e9d9da2dc873
Malcolm_Tyler <malcolm@malcsdomain.co.uk>
2025-05-08 18:40:36 +0100

    feat: Add ThemeDemo component showcasing theme and UI elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ThemeDemo from './ThemeDemo';
 
 function Home() {
   return (
     <div className="home-page">
       <h1>Sky Home Page</h1>
+      <ThemeDemo />
     </div>
   );
 }
