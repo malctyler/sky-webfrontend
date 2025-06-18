fa4f8c5f1fa09df52df4834aab8c48fb6e473792
Malcolm Tyler <malcolm@malcsdomain.co.uk>
2025-06-18 21:44:21 +0100

    feat: Refactor authentication handling by moving auth token management to cookies and updating related functions

---
+++ src/utils/cookieUtils.ts
@@ -0,0 +1,25 @@
+export const setCookie = (name: string, value: string, days: number = 7) => {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "; expires=" + date.toUTCString();
+    document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/; Secure; SameSite=Strict";
+};
+
+export const getCookie = (name: string): string | null => {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let c of ca) {
+        let cookiePair = c;
+        while (cookiePair.charAt(0) === ' ') {
+            cookiePair = cookiePair.substring(1);
+        }
+        if (cookiePair.indexOf(nameEQ) === 0) {
+            return decodeURIComponent(cookiePair.substring(nameEQ.length));
+        }
+    }
+    return null;
+};
+
+export const removeCookie = (name: string) => {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; Secure; SameSite=Strict';
+};
